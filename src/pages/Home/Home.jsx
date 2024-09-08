adef1df2930c4f0e731e4443eb186434cc8b1d36
Celine86 <celine.obringer@live.fr>
2024-09-08 15:19:56 +0200

    Add some precisions for the gameplay

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,6 +13,9 @@ export function Home() {
                         Carbone est un petit chat aventureux.<br /> 
                         Il a pris une grande décision !<br /> 
                         Partir à l'aventure pour voir la mer !<br />
+                        Nous sommes le 1er septembre.<br /> 
+                        Carbone peut être n'importe où sur la carte, son emplacement n'est pas indiquée par un point.<br />
+                        Suis son parcours et devine où il se trouve maintenant !<br />
                         Prêt à suivre les péripéties de Carbone et le retrouver ?<br />
                     </p>
                     <p className='homeNavlink'><NavLink className='generalNavlink' to='/board'>Oui, je suis prêt !</NavLink></p>
--- src/pages/HowTo/HowTo.jsx
+++ src/pages/HowTo/HowTo.jsx
@@ -9,6 +9,9 @@ export function HowTo() {
             <p>
                 Pour commencer clique sur le point que tu souhaites voir (1). Une carte d'info s'ouvre (2). Regarde où tu te trouves (3) et prends des notes (4).<br />
                 Tente ta chance en entrant le nom du lieu (5) et clique pour savoir si c'est juste (6).<br />
+                N'oublie pas que Carbone peut se trouver n'importe où !<br />
+                Son emplacement actuel n'est pas indiqué par un point !<br />
+                Suis son parcours et déduis en son amplacement !<br />
                 Tourne ton téléphone pour mieux voir l'image !
             </p>
             <p className='howtoNavlink'><NavLink className='generalNavlink' to='/board'>Je retourne jouer !</NavLink></p>
